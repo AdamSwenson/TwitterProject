369567da9fdbce691c8e0b1c1d8efb687a680e32
AdamSwenson <adam.swenson@csun.edu>
2019-01-14 11:14:53 -0800

    added afinn sentiment analysis library

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ VERSION = '0.0.1'
 # What packages are required for this module to be executed?
 REQUIRED = [
     'aiounittest', 'alembic',
+    'afinn',
     "ConfigParser",
     "dawg",
     "factory_boy",
